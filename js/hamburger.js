85fbefb059b69b6d624b03c77f1a28d30104f01a
mef51 <hms.mohammed@gmail.com>
2012-08-31 23:56:09 -0400

    Made open-close thing. Click they grey part in the middle

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
 
+    //make it so that when the "meat" div is clicked
+    // it opens.
+    makeOpenClose("meat", 500, 30, 400);
+
+
+
+    // ==================== Helpers ==================== //
+
+    function makeOpenClose(id, openHeight, closeHeight, duration) {
+        var isOpen = false;
+        $("#" + id).click(function(e){
+            if(!isOpen) {
+                $("#meat").animate({
+                    height: openHeight + "px"
+                }, duration, "swing");
+                isOpen = true;
+            }
+            else {
+                $("#meat").animate({
+                    height: closeHeight + "px"
+                }, duration, "swing");
+                isOpen = false;
+            }
+        });
+    }
+
 });
