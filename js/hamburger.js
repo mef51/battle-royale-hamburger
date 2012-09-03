f76af21d2a3e42801f0a57434f68ac458da4f151
mef51 <hms.mohammed@gmail.com>
2012-09-03 14:39:57 -0400

    Basic show/hide divs when the central part is clicked

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,30 +1,44 @@
 $(document).ready(function(){
 
+    init();
+
     //make it so that when the "meat" div is clicked
     // it opens.
-    makeOpenClose("meat", 500, 30, 400);
+    var isOpen = false;
+
+    var finalHeight = 500;
+    var initialHeight = 30;
+    var openDuration = 300; //ms
+    makeOpenClose("meat", finalHeight, initialHeight, openDuration);
 
 
 
     // ==================== Helpers ==================== //
 
+    function init() {
+        $("#meat").children().hide();
+    }
+
     function makeOpenClose(id, openHeight, closeHeight, duration) {
-        var isOpen = false;
         $("#" + id).click(function(e){
             if(!isOpen) {
+                isOpen = true;
+
                 $("#" +id).animate({
                     height: openHeight + "px"
                 }, duration);
 
-                isOpen = true;
+                $("#meat").children().show(400);
             }
             else {
+                isOpen = false;
+
+                $("#meat").children().hide(400);
+
                 $("#" + id).animate({
                     height: closeHeight + "px"
                 }, duration);
-                isOpen = false;
             }
         });
     }
-
 });
