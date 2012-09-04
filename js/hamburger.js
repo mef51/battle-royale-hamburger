b9d23d1d4d45038b00e792818733f778114ec840
mef51 <hms.mohammed@gmail.com>
2012-09-03 23:20:23 -0400

    Eventbrite styling

--- js/hamburger.js
+++ js/hamburger.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     // it opens.
     var isOpen = false;
 
-    var finalHeight = 1500;
+    var finalHeight = 1715;
     var initialHeight = 30;
     var openDuration = 600; //ms
     setTimeout(function(){
